fe5a40947cd11e3665c639cfa70e08f8dca08d06
Samarthya <87315517+samarthya04@users.noreply.github.com>
2024-11-02 21:38:52 +0530

    Update resume_parser.py

--- parsers/resume_parser.py
+++ parsers/resume_parser.py
@@ -1,8 +1,8 @@
-import fitz  # PyMuPDF
+import fitz
 
 def parse_resume(resume_file):
-    # Read the uploaded file directly
-    doc = fitz.open(stream=resume_file.read(), filetype="pdf")  # Specify the correct filetype
+    # Read the uploaded file
+    doc = fitz.open(stream=resume_file.read(), filetype="pdf")
     text = ""
     for page in doc:
         text += page.get_text()
